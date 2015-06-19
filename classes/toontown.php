431ddbaacd563008a90f0a3e3333126168a601e4
Adam romig <adam.romig@gmail.com>
2015-06-19 06:37:48 -0400

    Update
    Just new comments

--- classes/toontown.php
+++ classes/toontown.php
@@ -54,6 +54,7 @@ class ToonTown {
 		"The Big Cheese"=>array(8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49)
 		);
 
+	//  -- Set in ccg_ttr_vars now
 	public $districts = array(
 		"primary"=>"Pastel Plains",
 		"backup"=>array("Eraser Oasis", "Brush Bay")
@@ -66,7 +67,7 @@ class ToonTown {
 		"CEO"=>"+45 minutes"
 		);
 
-	// Run Schedule in Pacific Time
+	// Run Schedule in Pacific Time -- Set in ccg_ttr_schedule now
 	public $run_schedule = array(
 		"Monday"=>array(
 			"battle"=>array("CEO","VP","CFO","CJ"),
@@ -105,6 +106,7 @@ class ToonTown {
 			),
 		);
 
+	//  -- Set in ccg_ttr_vars now
 	public $beanfest_days = array(
 		"Saturdays"=>array("8:15 AM")
 		);
