74df64be014d6c12b6f342d2ef7eebd7ab1ba575
Adam Romig <adam.romig@gmail.com>
2015-06-22 11:26:43 -0400

    refined search result display

--- cp_search.php
+++ cp_search.php
@@ -5,30 +5,34 @@
 
 	$html = '';
 	$type = $_POST['type'];
-	$query = $_POST['query'];
+	$query = (!empty($_POST['query'])) ? $_POST['query'] : '%';
 
 	if ($type == 'user') {
+		$count = $db->count("ccg_users",array("mcf_username[~]"=>$query));
+
 		$results = $db->select("ccg_users",
 			array("mcf_username","mcf_userid"),
 			array("mcf_username[~]"=>$query,
 				  "ORDER"=>"mcf_username ASC")
 		);
 
-		$html .= '<div class="col-xs-12 well well-sm">';
+		$html .= '<div class="col-xs-12 well well-sm"><h6>'.$count.' user'.(($count > 1) ? 's' : '').' found.</h6>';
 		foreach ($results as $result) {
-			$html .= '<a class="btn btn-default user_result" id="'.$result['mcf_userid'].'" role="button" style="display: inline; margin-right: 6px;">'.$result['mcf_username'].'</a>';
+			$html .= '<a class="btn btn-default user_result" id="'.$result['mcf_userid'].'" role="button" style="margin-right: 6px; margin-bottom: 6px;">'.$result['mcf_username'].'</a>';
 		}
 		$html .= '</div>';
 	} elseif ($type == 'toon') {
+		$count = $db->count("ccg_toons",array("toon[~]"=>$query));
+
 		$results = $db->select("ccg_toons",
 			array("toon","toon_id"),
 			array("toon[~]"=>$query,
 				  "ORDER"=>"toon ASC")
 		);
 
-		$html .= '<div class="col-xs-12 well well-sm">';
+		$html .= '<div class="col-xs-12 well well-sm"><h6>'.$count.' toon'.(($count > 1) ? 's' : '').' found.</h6>';
 		foreach ($results as $result) {
-			$html .= '<a class="btn btn-default toon_result" id="'.$result['toon_id'].'" role="button" style="display: inline; margin-right: 6px;">'.$result['toon'].'</a>';
+			$html .= '<a class="btn btn-default toon_result" id="'.$result['toon_id'].'" role="button" style="margin-right: 6px; margin-bottom: 6px;">'.$result['toon'].'</a>';
 		}
 		$html .= '</div>';
 	}
