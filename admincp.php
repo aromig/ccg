65a686c4d49b8669133ef9515a4bc95422c332ac
Adam romig <adam.romig@gmail.com>
2015-06-30 18:44:08 -0400

    issue #8
    admincp - search toons by username

--- admincp.php
+++ admincp.php
@@ -228,11 +228,19 @@
             </div>
             <div id="toonMgr" class="tab-pane fade">
                 <h4>Manage Toons</h4>
-                <div class="col-xs-12 col-sm-8 col-md-6">
+                <div class="col-xs-12 col-sm-6 col-md-6">
                     <div class="input-group">
                         <input type="text" name="search_toon" id="search_toon" class="form-control" placeholder="Toon Name">
                         <span class="input-group-btn">
                             <button name="search_toon_btn" id="search_toon_btn" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-search"></span> Search</button>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                    <div class="input-group">
+                        <input type="text" name="search_userstoon" id="search_userstoon" class="form-control" placeholder="User Name">
+                        <span class="input-group-btn">
+                            <button name="search_userstoon_btn" id="search_userstoon_btn" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-search"></span> Search</button>
                         </span>
                     </div>
                 </div>
@@ -607,7 +615,154 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/admincp.js"></script>
+    <!--script src="js/admincp.js"></script-->
+    <script>
+$(document).ready(function(){
+    $("#user_details").hide();
+
+    $("#search_user").keypress(function(event){
+        if (event.which == 13) {
+            event.preventDefault();
+            $("#search_user_btn").trigger("click");
+        }
+    });
+    $("#search_user_btn").click(function(){
+        var query = $("#search_user").val();
+        var type = "user";
+        $.ajax({
+            type: "POST",
+            url: "cp_search.php",
+            data: "type="+type+"&query="+query,
+            success: function(html){
+                $("#user_results").html(html);
+            }
+        });
+    });
+
+    $("#user_results").on('click', '.user_result', function(){
+        var userid = $(this).prop('id');
+        $.ajax({
+            type: "POST",
+            url: "cp_user.php",
+            data: "userid="+userid,
+            success: function(json){
+                // populate form elements from json object
+                var user = $.parseJSON(json);
+                $("#ccg_id").val(user.ccg_id);
+                $("#orig_username").val(user.mcf_username);
+                $("#username").val(user.mcf_username);
+                $("#forum_title").html(user.mcf_title);
+                $("#reg_date").val(user.reg_date);
+                $("#reg_ip").val(user.reg_ip);
+                $("#last_login").val(user.last_login);
+                $("#last_ip").val(user.last_ip);
+                $("#bassie_val").val(user.bassie);
+                if (parseInt(user.bassie) === 1) { $("#bassie").prop('checked', true); } else { $("#bassie").prop('checked', false); }
+                if (parseInt(user.admin) === 1) { $("#admin").prop('checked', true); } else { $("#admin").prop('checked', false); }
+                $("#user_details").show();
+            }
+        });
+    });
+
+    $("#user_form").submit(function(){
+        var original_username = $("#username").val();
+        var new_username = $("#orig_username").val();
+
+        if (new_username != original_username) {
+            var response = confirm("You are about to change this user's username. This only should be done when the user's forum account has been renamed. Doing so otherwise will leave the user unable to access their profile.\r\n\r\nAre you sure you want to do this?");
+            if (!response) {
+                return false;
+            }
+        }
+    });
+
+    $("#trash_toon").click(function(){
+        var toonid = $("#toon_id").val();
+        var toonname = $("#toon").val();
+        var response = confirm("You are about to delete this toon. This operation cannot be undone.\r\n\r\nAre you sure you want to do this?");
+        if (!response) {
+            return false;
+        } else {
+            $.ajax({
+                type: "POST",
+                url: "cp_del_toon.php",
+                data: "toon_id="+toonid+"&toon="+toonname,
+                success: function(html){
+                    $("#toon_results").html(html);
+                    $("#toon_details").hide();
+                }
+            });
+        }
+    });
+
+    $("#toon_details").hide();
+
+    $("#search_toon").keypress(function(event){
+        if (event.which == 13) {
+            event.preventDefault();
+            $("#search_toon_btn").trigger("click");
+        }
+    });
+    $("#search_userstoon").keypress(function(event){
+        if (event.which == 13) {
+            event.preventDefault();
+            $("#search_userstoon_btn").trigger("click");
+        }
+    });
+    $("#search_toon_btn").click(function(){
+        var query = $("#search_toon").val();
+        var type = "toon";
+        $.ajax({
+            type: "POST",
+            url: "cp_search.php",
+            data: "type="+type+"&query="+query,
+            success: function(html){
+                $("#toon_results").html(html);
+            }
+        });
+    });
+    $("#search_userstoon_btn").click(function(){
+        var query = $("#search_userstoon").val();
+        var type= "toon";
+        $.ajax({
+            type: "POST",
+            url: "cp_search.php",
+            data: "type="+type+"&query="+query+"&user=1",
+            success: function(html){
+                $("#toon_results").html(html);
+            }
+        });
+    });
+
+    $("#toon_results").on('click', '.toon_result', function(){
+        var toonid = $(this).prop('id');
+        $.ajax({
+            type: "POST",
+            url: "cp_toon.php",
+            data: "toonid="+toonid,
+            success: function(json){
+                var toon = $.parseJSON(json);
+                $("#toon_id").val(toon.toon_id);
+                $("#toon").val(toon.toon);
+                $("#toon_username").val(toon.mcf_username);
+                $("#laff").val(toon.laff);
+                $("#gags_toonup").val(toon.max_toonup);
+                $("#gags_trap").val(toon.max_trap);
+                $("#gags_lure").val(toon.max_lure);
+                $("#gags_sound").val(toon.max_sound);
+                $("#gags_throw").val(toon.max_throw);
+                $("#gags_squirt").val(toon.max_squirt);
+                $("#gags_drop").val(toon.max_drop);
+                $("#suit_sell").val(toon.sellsuit);
+                $("#suit_cash").val(toon.cashsuit);
+                $("#suit_law").val(toon.lawsuit);
+                $("#suit_boss").val(toon.bosssuit);
+                $("#toon_details").show();
+            }
+        })
+    });
+});
+    </script>
 
   </body>
 </html>
--- cp_search.php
+++ cp_search.php
@@ -24,13 +24,26 @@
 		}
 		$html .= '</div>';
 	} elseif ($type == 'toon') {
-		$count = $db->count("ccg_toons",array("toon[~]"=>$query));
+		if (!isset($_POST['user'])) {
+			$count = $db->count("ccg_toons",array("toon[~]"=>$query));
 
-		$results = $db->select("ccg_toons",
-			array("toon","toon_id"),
-			array("toon[~]"=>$query,
-				  "ORDER"=>"toon ASC")
-		);
+			$results = $db->select("ccg_toons",
+				array("toon","toon_id"),
+				array("toon[~]"=>$query,
+					  "ORDER"=>"toon ASC")
+			);
+		} else {
+			$count = $db->count("ccg_toons",
+								array("[><]ccg_users"=>array("ccg_id"=>"ccg_id")),
+								"ccg_toons.toon",
+								array("ccg_users.mcf_username[~]"=>$query)
+								);
+			$results = $db->select("ccg_toons",
+								array("[><]ccg_users"=>array("ccg_id"=>"ccg_id")),
+								array("ccg_toons.toon","ccg_toons.toon_id"),
+								array("ccg_users.mcf_username[~]"=>$query)
+								);
+		}
 
 		$html .= '<div class="col-xs-12 well well-sm"><h6>'.$count.' toon'.(($count > 1) ? 's' : '').' found.</h6>';
 		foreach ($results as $result) {
